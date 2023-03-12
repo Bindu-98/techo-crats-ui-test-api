6d4436e27eae7562bdff31005fda815c75f44f1d
aruna <aruna@saastaq.io>
2023-03-12 08:04:52 +0530

    Intial Commit

---
+++ src/main/java/com/chathurya/uitestapi/UiTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.chathurya.uitestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chathurya/uitestapi/UiTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chathurya.uitestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
